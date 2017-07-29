effec9b09c2017e48a18be46987152ed1f209611
efios <imtkyo@gmail.com>
2017-07-29 03:20:49 +0200

    added a ndigits function

--- src/hex_edit.cpp
+++ src/hex_edit.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <fstream>
 
+int to_hex()
+{
+
+    return 0;
+}
+
+int ndigits(const int value)
+{
+    int i, temp;
+    temp = value;
+    for(i = 0; temp > 0; ++i)
+        temp = temp / 10;
+
+    return i;
+}
 
 int main()
 {
+    std::ofstream temp_name;
     const short buffer_size = 1024;
     char buffer[1024];
 
